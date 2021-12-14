018ceb3261169ea86b3fe9cfccce79448bd60ba3
Vitalii Brovdi <brovdi.vitaliy@gmail.com>
2021-12-14 13:23:14 +0200

    сhanged

---
+++ labspython/lab19v2/zavd3.py
@@ -0,0 +1,32 @@
+def input_one_song():
+    song = {}
+    song['group'] = input('Group: ')
+    song['singers'] = input('Singers: ').split(' ')
+    song['disk'] = input('Disk: ')
+    song['song'] = input('Song: ')
+    return song
+
+
+def input_songs():
+    n = int(input('Кількість пісень: '))
+    return [input_one_song() for i in range(n)]
+
+
+def search_song(songs_list, song_title):
+    return list(filter(lambda song: song['song'] == song_title, songs_list))
+
+
+# -----------------
+
+songs_list = input_songs()
+
+while True:
+    ans = input('Do you want to search {y/n}?')
+    if ans == 'n':
+        break
+    song_title = input('Song for search: ')
+    songs_res = search_song(songs_list, song_title)
+    if len(songs_res) > 0:
+        print(songs_res)
+    else:
+        print('No song')
