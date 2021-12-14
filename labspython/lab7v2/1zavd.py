bbf3cad500509baa1fd45bb823b91a523248c66f
Vitalii Brovdi <brovdi.vitaliy@gmail.com>
2021-12-14 13:11:57 +0200

    сhanged

--- labspython/lab7v2/1zavd.py
+++ labspython/lab7v2/1zavd.py
@@ -1,14 +1,15 @@
 import random
 import numpy as np
+
 sum = 0
 a = []
-n=int(input("Введіть кількість рядків матриці : "))
-m=int(input("Введіть кількість стовбців матриці : "))
+n = int(input("Введіть кількість рядків матриці : "))
+m = int(input("Введіть кількість стовбців матриці : "))
 for i in range(n):
-    a.append([float(random.randint(-10,10)) for j in range(m)])
+    a.append([float(random.randint(-10, 10)) for j in range(m)])
     for j in range(m):
-        if i % 2 != 0 and j % 2 == 0 and a[i][j]>0 :
-            sum +=a[i][j]
+        if i % 2 != 0 and j % 2 == 0 and a[i][j] > 0:
+            sum += a[i][j]
 a = np.array(a)
 print(a)
 print(sum)
--- labspython/lab7v2/2zavd.py
+++ labspython/lab7v2/2zavd.py
@@ -1,19 +1,21 @@
 import random
 import numpy as np
+
 a = []
 b = []
-n=int(input("Введіть кількість рядків матриці : "))
-m=int(input("Введіть кількість стовбців матриці : "))
+n = int(input("Введіть кількість рядків матриці : "))
+m = int(input("Введіть кількість стовбців матриці : "))
 for i in range(n):
-    a.append([float(random.randint(0,1)) for j in range(m)])
+    a.append([float(random.randint(0, 1)) for j in range(m)])
 a = np.array(a)
 print(a)
 for i in range(n):
     b.append([float(random.randint(0, 1)) for j in range(m)])
 b = np.array(b)
 print(b)
-for j in range(m):
-    if a[i][j] == 0:
-         b[i][j] = a[i][j]
+for i in range(n):
+    for j in range(m):
+        if a[i][j] == 0:
+            a[i][j] = b[i][j]
 a = np.array(a)
 print(a)
