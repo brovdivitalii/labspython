0b8d570c047a91f14a5f0e33005421251ebf13dc
Vitalii Brovdi <brovdi.vitaliy@gmail.com>
2021-12-14 13:04:33 +0200

    сhanged

--- labspython/lab6v2/zavd2.py
+++ labspython/lab6v2/zavd2.py
@@ -1,19 +1,18 @@
 b = []
 n = int(input('введіть кількість елементів = '))
 res = 1
-v1 = 0
 v2 = 1
 i1 = i2 = 1
 for i in range(1,n+1):
     if i % 2 == 0:
-        while i1 <= i:
-            v1 = v1 + 1/i1
-            i1 = i1 + 1
+        v1 = 1
+        for j in range(2, i + 1, 2):
+            v1 += 1 / j
         b.insert(i, v1)
     else:
         while i2 <= i:
             v2 = v2 * i2
-            sum= (v2/2) + 3
+            sum = (v2/2) + 3
             i2 = i2+1
         b.insert(i, sum)
 for i in range(1, n+1):
